02d7f576d63f7b40cff57a91063d4b6e0a16819f
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-05-06 20:12:27 +0200

    Initial commit

---
+++ src/OnugDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using OnugDemo.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnugDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBlogRepo _repo;
+
+        public HomeController(IBlogRepo repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var post = _repo.GetPost();
+
+            return View(post);
+        }
+    }
+}
---
+++ src/OnugDemo/Models/BlogRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnugDemo.Models
+{
+    public class BlogRepo : IBlogRepo
+    {
+        public BlogRepo()
+        {
+
+        }
+
+        public BlogPost GetPost()
+        {
+            var post = new BlogPost
+            {
+                PostId = 123,
+                Title = "ONUG ROCKS!",
+                Content = "ASP.NET 5 is also cool!"
+            };
+
+            return post;
+        }
+    }
+}
